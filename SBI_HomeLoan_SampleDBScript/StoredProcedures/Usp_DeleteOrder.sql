a4f306ff98071be145a0d0b9fafa8d91e2b2ae78
srikanthcbs123 <srikanthcbs123@gmail.com>
2025-03-18 15:13:03 +0530

    Usertory_34567 Orderssqlscript created

---
+++ SBI_HomeLoan_SampleDBScript/StoredProcedures/Usp_DeleteOrder.sql
@@ -0,0 +1,6 @@
+﻿Create procedure Usp_DeleteOrder(@orderid int)  
+as   
+begin  
+set nocount on--it prvents no of rows effected.   
+Delete from Orders where orderid=@orderid  
+end
---
+++ SBI_HomeLoan_SampleDBScript/StoredProcedures/Usp_UpdateOrder.sql
@@ -0,0 +1,6 @@
+﻿Create procedure Usp_UpdateOrder(@orderid int,@ordername varchar(max),@orderlocation varchar(max))  
+as   
+begin  
+set nocount on--it prvents no of rows effected.   
+Update Orders set ordername=@ordername,orderlocation=@orderlocation where orderid=@orderid  
+end
